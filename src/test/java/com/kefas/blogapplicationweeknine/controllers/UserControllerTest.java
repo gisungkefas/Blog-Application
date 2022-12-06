36822683fb504229fb2b3f7e0cf1fa7bdbfdeaf5
Kefas Gisung <gisungkefas@gmail.com>
2022-12-06 07:27:32 +0100

    Refactored my user test

--- src/test/java/com/kefas/blogapplicationweeknine/controllers/UserControllerTest.java
+++ src/test/java/com/kefas/blogapplicationweeknine/controllers/UserControllerTest.java
@@ -5,25 +5,28 @@ import com.kefas.blogapplicationweeknine.dto.UserDto;
 import com.kefas.blogapplicationweeknine.services.UserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ContextConfiguration(classes = {UserController.class})
-@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserController.class)
 class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
 
     @Autowired
     private UserController userController;
@@ -38,53 +41,29 @@ class UserControllerTest {
     @Test
     void testCreateUser() throws Exception {
         UserDto userDto = new UserDto();
-        userDto.setEmail("jane.doe@example.org");
-        userDto.setFirstname("Jane");
-        userDto.setLastname("Doe");
-        userDto.setPassword("iloveyou");
+        userDto.setEmail("lisa@gmail.com");
+        userDto.setFirstname("Lisa");
+        userDto.setLastname("Lott");
+        userDto.setPassword("1234");
         userDto.setPhoneNumber("4105551212");
-        when(userService.createUser((UserDto) any())).thenReturn(userDto);
 
-        UserDto userDto1 = new UserDto();
-        userDto1.setEmail("jane.doe@example.org");
-        userDto1.setFirstname("Jane");
-        userDto1.setLastname("Doe");
-        userDto1.setPassword("iloveyou");
-        userDto1.setPhoneNumber("4105551212");
-        String content = (new ObjectMapper()).writeValueAsString(userDto1);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/v1/user/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(userController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().isAccepted())
-                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andExpect(MockMvcResultMatchers.content()
-                        .string(
-                                "{\"firstname\":\"Jane\",\"lastname\":\"Doe\",\"email\":\"jane.doe@example.org\",\"phoneNumber\":\"4105551212\",\"password"
-                                        + "\":\"iloveyou\"}"));
+        String requestBody = mapper.writeValueAsString(userDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/user/register", 42L)
+                        .contentType("application/json").content(requestBody))
+                .andExpect(status().isAccepted());
     }
 
     @Test
     void testUserLogin() throws Exception {
-        when(userService.userLogin((UserDto) any())).thenReturn("User Login");
-
         UserDto userDto = new UserDto();
-        userDto.setEmail("jane.doe@example.org");
-        userDto.setFirstname("Jane");
-        userDto.setLastname("Doe");
-        userDto.setPassword("iloveyou");
-        userDto.setPhoneNumber("4105551212");
-        String content = (new ObjectMapper()).writeValueAsString(userDto);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/v1/user/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(userController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().isAccepted())
-                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
-                .andExpect(MockMvcResultMatchers.content().string("User Login"));
+        userDto.setEmail("lisa@gmail.com");
+        userDto.setPassword("1234");
+
+        String requestBody = mapper.writeValueAsString(userDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/user/login", 42L)
+                        .contentType("application/json").content(requestBody))
+                .andExpect(status().isAccepted());
     }
 }
